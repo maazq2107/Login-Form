8a97ebbff3a6bcd481f5a93ea12bb1828a081e92
maazq2107 <72332472+maazq2107@users.noreply.github.com>
2024-07-30 11:24:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [isLogin, setIsLogin] = useState(true);
+
+  return (
+    <div className="container">
+      <div className='form-container'>
+        <div className='form-toggle'>
+          <button className={isLogin ? 'active' : ""} onClick={() => setIsLogin(true)}>Login</button>
+          <button className={!isLogin ? 'active' : ""} onClick={() => setIsLogin(false)}>SignUp</button>
+        </div>
+        {isLogin ? <>
+          <div className='form'>
+            <h2>Login Form</h2>
+            <input type='email' placeholder='Email' />
+            <input type='password' placeholder='Password' />
+            <a href="#">Forgot Password?</a>
+            <button>Login</button>
+            <p>Not a Member? <a href='#' onClick={() => setIsLogin(false)}>Signup now</a></p>
+          </div>
+        </> : <>
+          <div className='form'>
+            <h2>SignUp Form</h2>
+            <input type='email' placeholder='Email' />
+            <input type='password' placeholder='Password' />
+            <input type='password' placeholder='Confirm Password' />
+            <button>SignUp</button>
+          </div>
+        </>}
+      </div>
+    </div>
+  );
+}
+
+export default App;
